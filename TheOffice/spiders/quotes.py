9b3f8b22d93d710af71fa27a72c5fb71dc86884c
Preston Badeer <preston@badeer.co>
2019-09-04 12:56:54 -0500

    Working fix for issue #1

--- TheOffice/spiders/quotes.py
+++ TheOffice/spiders/quotes.py
@@ -8,15 +8,12 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://www.officequotes.net/no1-01.php']
 
     def parse(self, response):
-
-        season = response.css('td[bgcolor="#FFF8DC"] b:first-of-type::text').get().split('-')[0]
-        episode = response.css('td[bgcolor="#FFF8DC"] b:first-of-type::text').get().split('-')[1]
-
+        season, episode = response.css('td[bgcolor="#FFF8DC"] b:first-of-type::text').get().split(' - ')
         title = response.css('td[bgcolor="#FFF8DC"] b:nth-of-type(2)::text').get()
-
         written_by = response.css('td[bgcolor="#FFF8DC"]::text').getall()[3].strip()
         directed_by = response.css('td[bgcolor="#FFF8DC"]::text').getall()[4].strip()
         transcribed_by = response.css('td[bgcolor="#FFF8DC"]::text').getall()[5].strip()
+        scenes = response.css('div.quote')
 
         # yield as a dictionary
         new_episode = {
@@ -29,37 +26,34 @@ class QuotesSpider(scrapy.Spider):
             'scenes': {}
         }
 
-        # use css selectors to get season and episode
+        # loop through scenes in episode
+        for index, scene in enumerate(scenes):
+            new_scene = {
+                'scene_number': index + 1,
+                'quotes': {}
+            }
 
+            scene_lines_text = scene.css('::text').getall()
+            scene_lines_cleaned = [q.strip() for q in scene_lines_text if q.strip() != '']
 
-        #loop through the response object and append the quotes to the scenes variable
-
-            # new_scene = {
-            #     'scene_number': 0,
-            #     'quotes': {}
-            # }
-
-            quotes = response.css('.quote')
-            quotes_text = response.css('.quote::text').getall()
-            quotes_prepared = [q.strip() for q in quotes_text if q.strip() != '']
-
-            for index, quote in quotes:
-                author = quote.css('b:first-of-type::text').get()[:-1]
-                quote = quotes_prepared[index]
-                new_scene['quotes'][index]({
-                    'author': author,
-                    'quote': quote
-                })
-
-        new_episode['scenes'][new_scene['scene_number']] = new_scene
+            # loop through every other line in scene (which is the line with the author name)
+            for index, text in enumerate(scene_lines_cleaned[::2]):
+                new_scene['quotes'][index] = {
+                    'author': text[:-1],
+                    'quote': scene_lines_cleaned[(index * 2) + 1]
+                }
+            
+            # append new scene to episode
+            new_episode['scenes'][new_scene['scene_number']] = new_scene
 
         yield new_episode
 
         # Take current url, split by / and get the last part (the page name, like asdf.php)
         current_page = str(response.request.url).split('/')[-1]
+
         # Get the first link that happens after our current link
         next_page = response.css(f'.navEp a[href="{current_page}"] ~ a:first-of-type::attr(href)').get()
-        if next_page is not None:
+        if next_page:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
 
